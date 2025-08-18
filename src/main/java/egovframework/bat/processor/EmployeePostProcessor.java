64ab1fd51f10df1d82a10af0275111da72095c0d
BulSae <darshu@naver.com>
2025-08-18 10:54:00 +0900

    feat: generate essential id and process employees

---
+++ src/main/java/egovframework/bat/processor/EmployeePostProcessor.java
@@ -0,0 +1,27 @@
+package egovframework.bat.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import egovframework.bat.domain.insa.EmployeeInfo;
+import egovframework.bat.domain.insa.EsntlIdGenerator;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 직원 정보에 고유ID와 소스시스템 값을 설정하는 Processor
+ */
+@Component
+@RequiredArgsConstructor
+public class EmployeePostProcessor implements ItemProcessor<EmployeeInfo, EmployeeInfo> {
+
+    private final EsntlIdGenerator esntlIdGenerator;
+    private static final String SOURCE_SYSTEM = "LOCAL";
+
+    @Override
+    public EmployeeInfo process(EmployeeInfo item) {
+        item.setEsntlId(esntlIdGenerator.generate());
+        item.setSourceSystem(SOURCE_SYSTEM);
+        return item;
+    }
+}
+

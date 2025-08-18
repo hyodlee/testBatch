fb6e73a06a4183e4132912bb5601cc9de56baeb8
BulSae <darshu@naver.com>
2025-08-18 13:47:41 +0900

    sourceSystem 관련 필드 및 컬럼 제거

--- src/main/java/egovframework/bat/domain/insa/EmployeeInfoProcessor.java
+++ src/main/java/egovframework/bat/domain/insa/EmployeeInfoProcessor.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.springframework.batch.item.ItemProcessor;
 
 /**
- * ESNTL_ID와 SOURCE_SYSTEM 값을 세팅하는 프로세서
+ * ESNTL_ID 값을 세팅하는 프로세서
  */
 public class EmployeeInfoProcessor implements ItemProcessor<EmployeeInfo, EmployeeInfo> {
 
@@ -13,8 +13,6 @@ public class EmployeeInfoProcessor implements ItemProcessor<EmployeeInfo, Employ
     public EmployeeInfo process(EmployeeInfo item) throws Exception {
         // 고유ID를 UUID로 생성
         item.setEsntlId(UUID.randomUUID().toString());
-        // 원천시스템 값을 STG로 설정
-        item.setSourceSystem("STG");
         return item;
     }
 }
--- src/main/java/egovframework/bat/processor/EmployeePostProcessor.java
+++ src/main/java/egovframework/bat/processor/EmployeePostProcessor.java
@@ -5,24 +5,21 @@ import org.springframework.stereotype.Component;
 
 import egovframework.bat.domain.insa.EmployeeInfo;
 import egovframework.bat.domain.insa.EsntlIdGenerator;
-import egovframework.bat.domain.insa.SourceSystemPrefix;
 import lombok.RequiredArgsConstructor;
 
 /**
- * 직원 정보에 고유ID와 소스시스템 값을 설정하는 Processor
+ * 직원 정보에 고유ID를 설정하는 Processor
  */
 @Component
 @RequiredArgsConstructor
 public class EmployeePostProcessor implements ItemProcessor<EmployeeInfo, EmployeeInfo> {
 
     private final EsntlIdGenerator esntlIdGenerator;
-    private static final String SOURCE_SYSTEM = "LOCAL";
 
     @Override
     public EmployeeInfo process(EmployeeInfo item) {
-        String prefix = SourceSystemPrefix.getPrefix(item.getSourceSystem());
-        item.setEsntlId(esntlIdGenerator.generate(prefix));
-        item.setSourceSystem(SOURCE_SYSTEM);
+        // ESNTL_ID를 생성하여 설정
+        item.setEsntlId(esntlIdGenerator.generate(""));
         return item;
     }
 }

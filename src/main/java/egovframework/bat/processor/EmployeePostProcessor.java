347bd810e325eab90c21251701a681adc386410c
BulSae <darshu@naver.com>
2025-08-18 13:58:05 +0900

    fix: 직원 정보 후처리 간소화

--- src/main/java/egovframework/bat/processor/EmployeePostProcessor.java
+++ src/main/java/egovframework/bat/processor/EmployeePostProcessor.java
@@ -1,28 +1,30 @@
 package egovframework.bat.processor;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import egovframework.bat.domain.insa.EmployeeInfo;
 import egovframework.bat.domain.insa.EsntlIdGenerator;
-import egovframework.bat.domain.insa.SourceSystemPrefix;
 import lombok.RequiredArgsConstructor;
 
 /**
- * 직원 정보에 고유ID와 소스시스템 값을 설정하는 Processor
+ * 직원 정보에 고유ID를 설정하는 Processor
  */
 @Component
 @RequiredArgsConstructor
 public class EmployeePostProcessor implements ItemProcessor<EmployeeInfo, EmployeeInfo> {
 
     private final EsntlIdGenerator esntlIdGenerator;
-    private static final String SOURCE_SYSTEM = "LOCAL";
+
+    // 외부에서 전달된 프리픽스 값
+    @Value("${source.system.prefix:}")
+    private String prefix;
 
     @Override
     public EmployeeInfo process(EmployeeInfo item) {
-        String prefix = SourceSystemPrefix.getPrefix(item.getSourceSystem());
+        // 전달받은 프리픽스를 이용해 ESNTL_ID 생성
         item.setEsntlId(esntlIdGenerator.generate(prefix));
-        item.setSourceSystem(SOURCE_SYSTEM);
         return item;
     }
 }

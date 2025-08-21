05c5342b312f342a3270ec5ce703a58dd6aca1d0
BulSae <darshu@naver.com>
2025-08-21 11:32:11 +0900

    EsntlIdGenerator를 util 패키지로 이동

--- src/main/java/egovframework/bat/insa/domain/EmployeeInfoProcessor.java
+++ src/main/java/egovframework/bat/insa/domain/EmployeeInfoProcessor.java
@@ -5,6 +5,8 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import egovframework.bat.insa.util.EsntlIdGenerator;
+
 import lombok.RequiredArgsConstructor;
 
 /**

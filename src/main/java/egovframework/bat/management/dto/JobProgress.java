56fb0b93703f448fcb2f324a2232e4850256a3e2
BulSae <darshu@naver.com>
2025-08-31 23:48:13 +0900

    feat: 배치 실행 중복 방지 및 진행 상황 SSE 제공

---
+++ src/main/java/egovframework/bat/management/dto/JobProgress.java
@@ -0,0 +1,16 @@
+package egovframework.bat.management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 배치 작업 진행 상황을 전달하기 위한 DTO.
+ */
+@Data
+@AllArgsConstructor
+public class JobProgress {
+    /** 작업 이름 */
+    private String jobName;
+    /** 현재 상태 */
+    private String status;
+}

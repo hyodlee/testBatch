ad1cb8dd0c3ea45ef1a39a96e168a7d0f8104a2e
BulSae <darshu@naver.com>
2025-09-08 22:25:23 +0900

    내구성 잡 일시중지/재개 제한 및 예외 처리

---
+++ src/main/java/egovframework/bat/management/exception/DurableJobPauseResumeNotAllowedException.java
@@ -0,0 +1,23 @@
+package egovframework.bat.management.exception;
+
+/**
+ * 내구성 잡의 일시 중지 또는 재개를 시도할 때 발생하는 예외.
+ */
+public class DurableJobPauseResumeNotAllowedException extends RuntimeException {
+
+    /**
+     * 기본 생성자.
+     */
+    public DurableJobPauseResumeNotAllowedException() {
+        super();
+    }
+
+    /**
+     * 예외 메시지를 포함한 생성자.
+     *
+     * @param message 예외 메시지
+     */
+    public DurableJobPauseResumeNotAllowedException(String message) {
+        super(message);
+    }
+}
